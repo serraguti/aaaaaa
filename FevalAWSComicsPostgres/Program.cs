3d91a8d5f08f185376e16ceb7e37f33a42d22a77
serraguti <pacoserranox@gmail.com>
2023-11-23 19:43:52 +0100

    d

--- FevalAWSComicsPostgres/Program.cs
+++ FevalAWSComicsPostgres/Program.cs
@@ -7,13 +7,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 //DEBEMOS INDICAR QUE VAMOS A UTILIZAR EL REPOSITORY EN NUESTRA APP
 builder.Services.AddTransient<RepositoryComics>();
+string connectionStringPostgres =
+    "Host=107.22.122.236;Port=5432;Username=postgres;Password=postgres;Database=miscomics;sslmode=disable";
 string connectionStringMySqlLocal =
     "server=localhost; port=3306; user id=root; password=mysql; database=ejemplo";
+string connectionStringMySqlAWS =
+    "server=awsmysqlpaco.cf5cqucmwlm6.us-east-1.rds.amazonaws.com; port=3306; user id=adminsql; password=Admin123; database=ejemplo";
 //ENVIAMOS LA CADENA DE CONEXION A NUESTRO CONTEXT PARA PODER 
 //COMUNICAR CON LA BASE DE DATOS
 builder.Services.AddDbContext<ComicsContext>
-    (options => options.UseMySql(connectionStringMySqlLocal
-    , ServerVersion.AutoDetect(connectionStringMySqlLocal)));
+    (options => options.UseNpgsql(connectionStringPostgres));
+//builder.Services.AddDbContext<ComicsContext>
+//    (options => options.UseMySql(connectionStringMySqlAWS
+//    , ServerVersion.AutoDetect(connectionStringMySqlAWS)));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
